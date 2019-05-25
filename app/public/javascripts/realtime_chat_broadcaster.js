ad42545298a51f4f9e08d8b2cd5b60883eaa0724
AshkanAbd <kotksthegame@gmail.com>
2019-05-25 05:02:26 +0430

    Access user media and record video and audio. NXT: try to send via socket.io

--- app/public/javascripts/realtime_chat_broadcaster.js
+++ app/public/javascripts/realtime_chat_broadcaster.js
@@ -3,4 +3,22 @@ $(function () {
     socket.on('msg', function (msg) {
         $('#messages').append($('<li>').text(msg));
     });
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+        const videoPlayer = document.getElementById('video_player');
+
+        navigator.getMedia({
+            video: true, audio: true
+        }, function (stream) {
+            videoPlayer.srcObject = stream;
+            videoPlayer.play()
+        }, function (err) {
+            alert("Can't access media devices");
+        });
+
+        return false;
+    })
 });
